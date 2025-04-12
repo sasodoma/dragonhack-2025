578432ebe328b0070ece23bc0feccad272be5448
plocnik <mr0459@student.uni-lj.si>
2025-04-12 18:37:51 +0200

    zacetek frontenda

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './styles.css';
+import React, { useState } from "react";
+import { Card } from './Card.js';
+
+export default function App() {
+
+
+  return (
+    <div className="main">
+      <Card />
+    </div>
+  );
+}
---
+++ frontend/src/Card.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export function Card() {
+
+
+  return (
+    <div className="card">
+      hehe +
+    </div>
+  );
+}
